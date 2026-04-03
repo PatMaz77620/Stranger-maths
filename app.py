be7a4f9ff05cca6c1fe9801dec3cdcf047048640
PatMaz77620 <patrick.masurel@hopaze.fr>
2026-04-03 11:16:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -622,7 +622,7 @@ elif st.session_state.page == 'chap4':
             st.write("Si $P(A)=0,3$ ou 30%, alors $P(\\bar{A})=0,7$ ou 70%.")
 
         st.write("")
-        st.write("🤔 La probabilité te dit ce qui devrait arriver en théorie, et la fréquence te dit ce qui est arrivé en pratique sur un échantillon donné. Mais les formules pour les calculer sont les mêmes !")
+        st.write("🤔 La probabilité te dit ce qui devrait arriver en théorie (dans le futur), et la fréquence te dit ce qui est arrivé (dans le passé) sur un échantillon donné. Mais les formules pour les calculer sont les mêmes !")
         
         st.divider()
         st.subheader("📡 Probabilités Conditionnelles")
