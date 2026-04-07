5677d1c8346f92b0a1ca8b0b2c1e1994021b6527
PatMaz77620 <patrick.masurel@hopaze.fr>
2026-04-07 09:04:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ genai.configure(api_key=GENAI_KEY)
 
 # Utilisation du nom complet 'models/...' 
 # C'est souvent la solution quand le nom court renvoie une 404
-model = genai.GenerativeModel('gemini-2.0-flash-lite')
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
 # --- 1. CONFIGURATION DE LA PAGE ---
 st.set_page_config(
